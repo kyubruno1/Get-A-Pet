d3f413d2de90665146e136879b49e715bc03a612
Bruno Ribeiro <kyubruno@gmail.com>
2022-06-21 16:03:36 -0300

    Criação de paginas Home, Login, Register e mudança no css

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,9 +1,30 @@
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom'
+
+/* pages */
+import Login from './components/pages/Auth/Login'
+import Register from './components/pages/Auth/Register'
+import Home from './components/pages/Home'
+
 
 function App() {
   return (
-    <div className="App">
-      <h1>Get A Pet</h1>
-    </div>
+    <Router>
+      <Switch>
+        <Route path="/login"> 
+          <Login />
+        </Route>
+      </Switch>
+      <Switch>
+        <Route path="/register"> 
+          <Register />
+        </Route>
+      </Switch>
+      <Switch>
+        <Route path="/"> 
+          <Home />
+        </Route>
+      </Switch>
+  </Router>
   );
 }
 
